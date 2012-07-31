c33f580ecb63f871a69242ea0ae2c6b4c5892895
forest gengfo <gengfo@gmail.com>
2012-07-31 21:53:46 +0800

    unit test

---
+++ UnitTestWithReport/src/main/java/gengfo/maven/App.java
@@ -0,0 +1,13 @@
+package gengfo.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
