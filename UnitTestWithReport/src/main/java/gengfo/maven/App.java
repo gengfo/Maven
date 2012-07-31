097dc4661848cf312ad1533a2c65e541056eb69a
forest gengfo <gengfo@gmail.com>
2012-08-01 00:12:27 +0800

    update maven check

--- UnitTestWithReport/src/main/java/gengfo/maven/App.java
+++ UnitTestWithReport/src/main/java/gengfo/maven/App.java
@@ -2,12 +2,13 @@ package gengfo.maven;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args)
+
+	{
+		int i = 0;
+		System.out.println("Hello World!");
+	}
 }
